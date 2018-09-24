e3a07293cb74704f1af152703c8cfef17a048e31
hannahhoward <hannah@hannahhoward.net>
2018-09-24 00:36:57 -0700

    Package fixups

--- rollup.config.ts
+++ rollup.config.ts
@@ -17,7 +17,7 @@ export default {
   ],
   sourcemap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: ["rxjs", "react", "@rxreact/core"],
+  external: ["rxjs"],
   watch: {
     include: "src/**"
   },
